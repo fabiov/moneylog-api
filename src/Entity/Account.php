7cc60eb04d44ef99c86c98e8c1bab50385e0fd7b
Fabio Ventura <fabio.ventura@softwave.it>
2019-11-22 11:29:48 +0100

    add account entity

---
+++ src/Entity/Account.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\AccountRepository")
+ * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(name="user_account_idx", columns={"user_id", "name"})})
+ */
+class Account
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $recap;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRecap(): ?bool
+    {
+        return $this->recap;
+    }
+
+    public function setRecap(bool $recap): self
+    {
+        $this->recap = $recap;
+
+        return $this;
+    }
+}

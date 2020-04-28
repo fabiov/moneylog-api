8b13fe1a19e00d7e45cf765a3a87451c0fc22351
Fabio Ventura <foravatenubi@gmail.com>
2020-04-28 11:38:39 +0200

    user test

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ApiResource()
+ * @ApiResource(collectionOperations={"post"})
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User
